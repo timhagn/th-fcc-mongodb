9f5691b308cdfacee093051679b54675061a6dc3
timhagn <mail@timhagn.com>
2018-09-26 13:15:06 +0200

    fix Person

--- myApp.js
+++ myApp.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 // <Your code here >
 
-//let Person /* = <Your Model> */
+let Person /* = <Your Model> */
 
 // **Note**: GoMix is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
